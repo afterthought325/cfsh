1e506491163092a48e4d801cd97743783bc6851f
Chaise Farrar (wcf74) <wcf74@msstate.edu>
2018-02-08 09:32:37 -0600

    Replaced circ_str_buf lib with simple array of pointers and some simple math. much easier to work with and cleaner. History now works. Valgrind tested for no memleaks.
     On branch master
     Changes to be committed:
    	renamed:    chaisesh.c -> cfsh.c
    	deleted:    circ_str_buf.c
    	deleted:    circ_str_buf.h

--- cfsh.c
+++ cfsh.c
@@ -13,6 +13,8 @@ Simple Shell written in C. Can handle the following shell features:
     - pwd
     - clear
     - ect....
+
+    History is stored in ./.cfsh_hist
 */
 
 #include <stdio.h>
@@ -21,23 +23,25 @@ Simple Shell written in C. Can handle the following shell features:
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
 
+#define MAX_HISTORY 10
+#define LINE_SIZE   80
 
-#define MAX_HISTORY 200
 
 char *prompt="$ >";
 
 int main (void) 
 {
-    pid_t   childPid,wPid;
-    char    status = 0;
-    char    line[81];
+    pid_t   childPid;
+    int     status = 0;
+    char    line[LINE_SIZE + 1];
     char    *token;
     char    *sep = " \t\n";
     char    **args;
-    char    **history;
-    int     hist_iter;
+    char    *history[MAX_HISTORY]; //the +1 is for the \0.
+    int     hist_iter = 0;
     
     
     /* 
@@ -48,42 +52,45 @@ int main (void)
     // Set max input for line
     args = malloc (80*sizeof( *args));
 
-    //Allocate Space for history
-    history = malloc(MAX_HISTORY*sizeof( *history));
-
-    //If history was successfully allocated, allocate enough spots for its items.
-    if (history) {
-        for (int x = 0; x < MAX_HISTORY; x++) {
-            history[x] = malloc(sizeof(history[x]));
-        }
+    for (int i = 0; i < MAX_HISTORY; i++) {
+        history[i] = (char *) NULL;
     }
 
     fprintf(stderr, "%s", prompt);
 
     while (fgets(line,80,stdin) != NULL) {
-        //Copy command into history array.
-        strcpy(history[hist_iter],line);
-        hist_iter = ++hist_iter % MAX_HISTORY; //Possibly the SegFault issue?
         int token_count = 0;
         token = strtok(line, sep);
         args[token_count++] = token;
+        if (args[0] == NULL) {
+            fprintf(stderr, "%s", prompt);
+            continue;
+        }
 
         while (token != NULL) {
             token = strtok(NULL, sep);
             args[token_count++] = token;
         }
-        //Add null pointer to keep array a valid C string.
-        args[token_count] = (char *) NULL;
+        
+        //Store command into history array.
+        if (history[hist_iter] == (char *) NULL) {
+            history[hist_iter] = malloc(LINE_SIZE + 1);
+        }
+        strcpy(history[hist_iter],line);
+        ++hist_iter;
+        hist_iter = hist_iter % (MAX_HISTORY - 1); 
 
         if (strcmp(args[0], "exit") == 0) {
             exit(0);
         } else if (strcmp(args[0], "history") == 0) {
-            //TODO: Fix this crap
-            for (int i = 0; i <= hist_iter; i++) {
-                fprintf(stderr,"%d: %s\n",i,history[i]);
+            for (int i = hist_iter + 1; i != (hist_iter); i++) { 
+
+                i = i % (MAX_HISTORY -1); //Loop back around to beginning of history array. Basically a simple Ring buffer.
+                if (history[i]) {
+                    fprintf(stderr,"- %s\n",history[i]);
+                }
             }
-        }
-        else {
+        } else {
             switch (childPid = fork()) {
             case 0:  execvp(args[0], args);   /* child */
                      fprintf(stderr, "ERROR %s no such program\n", line);
@@ -91,15 +98,19 @@ int main (void)
                      break;
             case -1: fprintf(stderr, "ERROR can't create child process!\n");   /* unlikely but possible if hit a limit */
                      break;
-            default: wPid = wait(&status);   /* Waits for all child processes */
+            default: wait(&status);   /* Waits for all child processes */
             }
-            fprintf(stderr, "%s", prompt);
         }
+
+        fprintf(stderr, "%s", prompt);
 
     }
     free(args);
-    for (int x = 0; x < MAX_HISTORY; x++) {
-        free(history[x]);
+
+    for (int i = 0; i < MAX_HISTORY; i++) {
+        if(history[i] != (char *) NULL) {
+            free(history[i]);
+        }
     }
-    free(history);
+
 }
